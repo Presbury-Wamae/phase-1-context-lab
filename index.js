168319747016aa7e221f3a4200e169d768fd4f58
Presbury <wamaepresbury034@gmail.com>
2025-03-31 19:10:23 +0300

    refactor: implement employee record management and payroll calculations

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
-// payroll.js
-
-class Payroll {
-    constructor() {
-        this.employees = [];
-    }
-
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-
-    calculatePay(employeeId) {
-        const employee = this.employees.find(emp => emp.id === employeeId);
-        if (!employee) throw new Error("Employee not found");
-        
-        return employee.hourlyRate * employee.hoursWorked;
-    }
-
-    totalPayroll() {
-        return this.employees.reduce((total, emp) => total + (emp.hourlyRate * emp.hoursWorked), 0);
-    }
-
-    generatePayslip(employeeId) {
-        const employee = this.employees.find(emp => emp.id === employeeId);
-        if (!employee) throw new Error("Employee not found");
-        
-        return `Payslip for ${employee.name}: \nHours Worked: ${employee.hoursWorked}\nHourly Rate: $${employee.hourlyRate}\nTotal Pay: $${this.calculatePay(employeeId)}`;
-    }
+// helpers.js
+function createEmployeeRecord(employeeData) {
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
 }
 
-module.exports = Payroll;
+function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+}
 
+function createTimeInEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
 
+function createTimeOutEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+function allWagesFor() {
+    const dates = this.timeInEvents.map(event => event.date);
+    return dates.reduce((total, date) => total + wagesEarnedOnDate.call(this, date), 0);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+}
+
+// Export functions if needed (for Node.js environment)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        createEmployeeRecord,
+        createEmployeeRecords,
+        createTimeInEvent,
+        createTimeOutEvent,
+        hoursWorkedOnDate,
+        wagesEarnedOnDate,
+        allWagesFor,
+        calculatePayroll,
+        findEmployeeByFirstName
+    };
+}
